4d528a61d5cd155eb63e0464a9807b055233daef
Haseeb1604 <haseeb200khan@gmail.com>
2022-01-26 14:09:30 +0500

    Introduction Video Issue Resolved with an additional support to start using javascript

--- js/script.js
+++ js/script.js
@@ -1,33 +1,39 @@
-(function ($) {
+(function () {
   'use strict';
+
+  let _ = selector =>{
+    const el =  document.querySelectorAll(selector);
+    return el.length > 1 ? el : el[0];
+  }
 
   /* ========================================================================= */
   /*	Page Preloader
   /* ========================================================================= */
-
   window.addEventListener('load', ()=>{
     setInterval(function(){ 
-      let preloader = document.getElementById('preloader');
+      let preloader = _('#preloader');
       preloader.style.display = 'none';
       preloader.style.transition = 'all 1s ease';
-    }, 500);
-    
+    }, 400);
+    let video = _('.introVideo');
+    video.mute();
+    video.play();
   })
   
 
   //Hero Slider
-  $('.hero-slider').slick({
-    autoplay: true,
-    infinite: true,
-    arrows: true,
-    prevArrow: '<button type=\'button\' class=\'prevArrow\'></button>',
-    nextArrow: '<button type=\'button\' class=\'nextArrow\'></button>',
-    dots: false,
-    autoplaySpeed: 7000,
-    pauseOnFocus: false,
-    pauseOnHover: false
-  });
-  $('.hero-slider').slickAnimation();
+  // $('.hero-slider').slick({
+  //   autoplay: true,
+  //   infinite: true,
+  //   arrows: true,
+  //   prevArrow: '<button type=\'button\' class=\'prevArrow\'></button>',
+  //   nextArrow: '<button type=\'button\' class=\'nextArrow\'></button>',
+  //   dots: false,
+  //   autoplaySpeed: 7000,
+  //   pauseOnFocus: false,
+  //   pauseOnHover: false
+  // });
+  // $('.hero-slider').slickAnimation();
 
 
   /* ========================================================================= */
@@ -35,12 +41,9 @@
   /* ========================================================================= */
   window.addEventListener('scroll', () =>{
     let scroll = window.pageYOffset;
-    let navbar = document.querySelector('.navigation');
-    if(scroll > 200) {
-        navbar.classList.add("sticky-header")
-    }else{
-        navbar.classList.remove("sticky-header")
-    }
+    let navbar = _('.navigation');
+    if(scroll > 200) navbar.classList.add("sticky-header")
+    else             navbar.classList.remove("sticky-header")
   });
 
-})(jQuery);
+})();
